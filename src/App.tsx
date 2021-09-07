fa6bf4fc83208d50bce5ff615332cc750aa51a51
Ruslan Shashkov <rshashkov@icloud.com>
2021-09-07 21:03:45 +0300

    Upd

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,12 @@ import { Selectable } from './lib/Selectable';
 function App() {
   return (
     <div className='App'>
-      <header className='Header'>Selectable Demo – Drag anywhere to start selecting</header>
+      <header className='Header'>
+        Selectable Demo – Drag anywhere to start selecting
+        <div className='Links'>
+          <a href='https://github.com/worx-to/drag-tools'>GitHub</a>
+        </div>
+      </header>
       <main className='Content'>
         <Selectable selectAreaClassName='Area' />
       </main>
